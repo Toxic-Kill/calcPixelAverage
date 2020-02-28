95e1474edb3b9d96aff9ab35872fbb98ae8453ca
Kill Toxic <913087364@qq.com>
2020-02-28 14:36:40 +0800

    添加注释

--- calcPixelAverage/calcPixelAverage.cpp
+++ calcPixelAverage/calcPixelAverage.cpp
@@ -9,16 +9,16 @@ int main()
 	cv::Mat M1 = cv::imread("D:\\Files\\2.jpg");//读取图片
 	int height = M1.rows;
 	int wide = M1.cols;
-	for (int i = 0; i < height; i++)
+	for (int i = 0; i < height; i++)//遍历每个像素
 	{
 		for (int j = 0; j < wide; j++)
 		{
-			uchar average = (M1.at<Vec3b>(i, j)[0] + M1.at<Vec3b>(i, j)[1] + M1.at<Vec3b>(i, j)[2]) / 3;
-			M1.at<Vec3b>(i, j)[0] = average;
-			M1.at<Vec3b>(i, j)[1] = average;
-			M1.at<Vec3b>(i, j)[2] = average;
+			uchar average = (M1.at<Vec3b>(i, j)[0] + M1.at<Vec3b>(i, j)[1] + M1.at<Vec3b>(i, j)[2]) / 3;//计算各像素点RGB平均值
+			M1.at<Vec3b>(i, j)[0] = average;//赋值
+			M1.at<Vec3b>(i, j)[1] = average;//赋值
+			M1.at<Vec3b>(i, j)[2] = average;//赋值
 		}
 	}
-	cv::imshow("M1", M1);
+	cv::imshow("M1", M1);//显示图片
 	waitKey(0);
 }
